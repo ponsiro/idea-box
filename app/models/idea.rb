9618bfcc2aa831aaefbf97b3ae14248f8c03f37c
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-16 09:47:24 +0900

    アソシエーションの追加と修正

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,9 @@ class Idea < ApplicationRecord
   validates :content, presence: true
   
   belongs_to :user
+  has_many :likes
   has_many :comments
   has_many :idea_tags
-  has_many :tags, through: :ideat_tags
+  has_many :tags, through: :idea_tags
+  
 end
