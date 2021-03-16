36c2da420e2328b6948eb4a7e9592278c94fe093
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-16 12:24:02 +0900

    ユーザー登録の項目の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :nickname, presence: true
+  validates :name, presence: true
 
   has_many :ideas
   has_many :comments
