04bf7379916f11fe5bbff0c8cd13f7f48a85c8cb
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-18 16:09:13 +0900

    投稿詳細ページの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users 
   root 'ideas#index'
   resources :ideas do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show]
   post 'like/:id' => 'likes#create', as: 'create_like'
