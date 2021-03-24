5bb41eb4f245261d83110cc74baeb685da4ef32d
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-24 16:41:53 +0900

    userアイコンの設定の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:email, :encrypted_password, :nickname, :name])
+                                      keys: [:email, :encrypted_password, :nickname, :name, :icon, :profile])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
+  before_action :check_user, only: [:edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @ideas = @user.ideas.order(created_at: "desc")
+  end
+
+  def edit
+    @image = @user.icon
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :nickname, :encrypted_password, :email, :icon, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_user
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_to root_path
+    end
   end
 end
