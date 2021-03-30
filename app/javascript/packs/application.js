9113d9896db23f725a1e79b33d811fe23fe823c8
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-30 17:57:06 +0900

    iconプレビューの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("../preview")
+require("../icon_preview")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
