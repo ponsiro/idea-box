e3a8d8cb694d2d45f737944b634d30fbc5ccd27c
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-31 15:59:27 +0900

    nicknameのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: {maximum: 20}
   validates :name, presence: true
   validates :profile, length: {maximum: 300}
 
